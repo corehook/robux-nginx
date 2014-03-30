e069e3a5e8250194e7c6fc26d50b6fe4594fc09f
Auanassov Chingiz <auanassov.chingiz@gmail.com>
2014-03-30 14:21:10 +0600

    Update

--- recipes/remove.rb
+++ recipes/remove.rb
@@ -1,5 +1,17 @@
+bash "add nginx repo and gpg key to system" do
+  user node.user
+  group node.group
+  ignore_failure true
+  code <<-EOC
+    sudo wget http://nginx.org/keys/nginx_signing.key -O /tmp/nginx.key
+    sudo apt-key add /tmp/nginx.key
+    sudo rm /etc/apt/sources.list.d/nginx.list
+    sudo apt-get update -y --yes --force-yes
+  EOC
+end
+
 package 'nginx' do 
-  action :remove  
+  action :purge 
 end
 
 include_recipe "robux-nginx::remove_configs"
